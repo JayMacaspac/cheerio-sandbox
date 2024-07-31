d95e97aa83c90c749086ac142c47930923746910
jaymacaspac <jaymacaspac08@gmail.com>
2024-07-31 09:57:00 +0800

    update template

--- src/index.js
+++ src/index.js
@@ -331,8 +331,53 @@ const formatDate = (e, a) => {
 // you can use 1 element to scrape depends on the html structure
 
 // return $("1st element, 2nd element").map((e, a) => {
+// // scraper here 
+// }).get().reduce((rows, item) => {
+//     const { home_team, away_team } = item
+//     const home_team_name = home_team.replace(/\([^)]+\)|\[[^\]]+\]/g, '').trim()
+//     const away_team_name = away_team.replace(/\([^)]+\)|\[[^\]]+\]/g, '').trim()
 
-// }).get()
+//   if (rows.find(
+//     (row) => row.league_name == item.league_name && row.date == item.date && row.home_team == home_team_name  && row.away_team == away_team_name
+//   )) {
+//     return rows.map((row) => {
+//       if (row.league_name == item.league_name && row.date == item.date && row.home_team == home_team_name && row.away_team == away_team_name) {
+//         row.odds.push({
+//           type: item.type,
+//           home: parseFloat(item.home),
+//           point: item.point,
+//           away: parseFloat(item.away),
+//         });
+//       }
+//       return row;
+//     });
+//   } else {
+//     const translation = getTeamNameTranslations(home_team_name, away_team_name, teamData)
+//     return [
+//       ...rows,
+//       {
+//           league_name: item.league_name,
+//           date: item.date,
+//           home_team: home_team_name,
+//           english_home_team: translation.home,
+//           away_team: away_team_name,
+//           english_away_team: translation.away,
+//           site_id: siteID,
+//           sport: item.sport,
+//           site_name: siteName,
+//           odds: [
+//             {
+//               type: item.type,
+//               home: parseFloat(item.home),
+//               point: item.point,
+//               away: parseFloat(item.away),
+//             }
+//           ]
+//       }
+//     ];
+    
+//   }
+// }, []);
 
 function getTeamNameTranslations(home_team_name, away_team_name, teamData) {
     return {
